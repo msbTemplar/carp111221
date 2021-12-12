41f713937bc8ee72d0a4d637051008e90b735bc2
simo <simo@simo.com>
2021-12-12 08:02:32 +0100

    modif121221

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-12-12 06:57:07 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-12-12T06:57:07+00:00	INFO 80.91.82.9	joomlafailure	Empty password not allowed.
